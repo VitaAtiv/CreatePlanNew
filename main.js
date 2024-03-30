4d018a26d7809d87dafbb89f53162fb1c71d8701
VitaAtiv <142582763+VitaAtiv@users.noreply.github.com>
2024-03-30 15:26:35 +0200

    add

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import './style.css'
 
+// 2.
+const LOCALSTORAGE_KEY = "todos"; // створюємо спеціальний ключ, аби дата записати інакше
+
+// 1.
 const refs = {
   formToDo: document.querySelector(".form-todo"),  // знаходимо кнопку
 };
@@ -9,9 +13,14 @@ function onBtnClick(event) {
   event.preventDefault()  // щоб не оновлювалась сторінка
   const formEl = event.currentTarget // знаходимо елемент форми, на якому відпрацювала відправка сабміт  
   console.log(formEl);
- const data = new FormData(formEl);  // створюється обєкт з властивостями-значеннями введеними в інпут та текстаріа
+ const formDataValue = new FormData(formEl);  // створюється обєкт з властивостями-значеннями введеними в інпут та текстаріа
+  const data = {};
+  formDataValue.forEach((value, name) => {
+    //  console.log(name, value);  // перебираємо кожен єлемент об'єкту і в консоль ключ та значення
+    data[name] = value; // в об'єкті дейт звертаюсь до неіснуючої властивості яка записується в змінну нейм. зберігаю в неї велью
+    // тобто створюється нова властивість нейм в дата і тудизаписується значення велью
+    // так відбувається спочатку для інпуту, потім для текстаріа
+  });
   console.log(data);
-  data.forEach((value, name) => {
-    console.log(name, value);
-  }) 
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
 }
